645c5c90c56ec5aaa08eefe707053e6325f82e3a
Homero de Jesús Portillo Calva <115849319+Homero117@users.noreply.github.com>
2024-10-12 14:32:42 -0600

    Una vez que un efecto visual es activado desde el  Dashboard este se debe ejecutar infinitamente  hasta que se envié un comando diferente o se envié  un comando para detenerlo.

--- 2024_remote_version_ESP32_MQTT/2024_remote_ESP32_MQTT_copy_20241006112541.ino
+++ 2024_remote_version_ESP32_MQTT/2024_remote_ESP32_MQTT_copy_20241006112541.ino
@@ -2,30 +2,30 @@
 #include <PubSubClient.h>
 
 // Credenciales Wi-Fi
-
-//const char* ssid = "Edificio_6";
-//const char* password = "50p0rt31t503h";
-const char* ssid = "INFINITUM2E26";
-const char* password = "237jQnRKYz";
-const char* mqtt_server = "broker.hivemq.com";
+const char* ssid = "INFINITUM2E26_78EXT";
+const char* password = "237jQnRKYz80";
+const char* mqtt_server = "192.168.1.105";
 
 // Objetos WiFi y MQTT
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Pines de botones
-const int btn1 = 15;
-const int btn2 = 16;
+const int btn1 = 32;
+const int btn2 = 33;
 
 // Variables de LEDs y estado
-int Led1= 2;
-int Led2= 4;
-int Led3= 5;
-int Led4= 18;
-int Led5= 19;
-int Led6= 21;
+int Led1 = 2;
+int Led2 = 4;
+int Led3 = 5;
+int Led4 = 18;
+int Led5 = 19;
+int Led6 = 21;
 
 const int LedS[] = {2, 4, 5, 18, 19, 21};
+
+// Variable global para almacenar el mensaje MQTT
+String messageT;
 
 void setup() {
   Serial.begin(115200);
@@ -48,12 +48,11 @@ void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
 }
 
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
+  // Conexión a la red Wi-Fi
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -75,48 +74,74 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
-  String messageT;
-  
+  messageT = "";  // Limpiar el contenido anterior
+
   for (int i = 0; i < length; i++) {
     Serial.print((char)message[i]);
     messageT += (char)message[i];
   }
   Serial.println();
 
-
   if (String(topic) == "esp32/homero") {
     Serial.print("Changing output to ");
-    if(messageT == "7 0"){
-      for(int x=0; x<6; x++){
-        digitalWrite(LedS[x-1],LOW);
-      }
+  }
+}
 
+void reconnect() {
+  // Reintentar conexión
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("Homero")) {
+      Serial.println("connected");
+      client.subscribe("esp32/homero");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
     }
-    if(messageT == "7 1"){
-      for(int x=6; x>0; x--){
-        digitalWrite(LedS[x-1],HIGH);
-      }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+  // Procesar el contenido de messageT
+  if(messageT == "7 0"){
+    for(int x = 0; x < 6; x++) {
+      digitalWrite(LedS[x], LOW);
     }
-// derecha a izquierda
-    if(messageT == "8 0"){
-      for(int x=0; x<6; x++){
-        digitalWrite(LedS[x-1],HIGH);
-        delay(100);
-        digitalWrite(LedS[x-1],LOW);
-        delay(100);
-      }
+  }
+
+  if(messageT == "7 1"){
+    for(int x = 6; x > 0; x--) {
+      digitalWrite(LedS[x], HIGH);
     }
-// izquierda -> derecha
-    if(messageT == "8 1"){
-      for(int x=6; x>0; x--){
-        digitalWrite(LedS[x-1],HIGH);
-        delay(100);
-        digitalWrite(LedS[x-1],LOW);
-        delay(100);
-      }
+  }
+  
+  // derecha a izquierda
+  if(messageT == "8 0"){
+    for(int x = 0; x < 6; x++) {
+      digitalWrite(LedS[x], HIGH);
+      delay(100);
+      digitalWrite(LedS[x], LOW);
+      delay(100);
     }
-// serial derecha a iz
-    if(messageT == "9 0"){
+  }
+
+  // izquierda a derecha
+  if(messageT == "8 1"){
+    for(int x = 6; x > -1; x--) {
+      digitalWrite(LedS[x], HIGH);
+      delay(100);
+      digitalWrite(LedS[x], LOW);
+      delay(100);
+    }
+  }
+  if(messageT == "9 0"){
       digitalWrite(Led1, HIGH);
       delay(100);
       digitalWrite(Led2, HIGH);
@@ -167,10 +192,9 @@ void callback(char* topic, byte* message, unsigned int length) {
           digitalWrite(Led6, LOW);
           delay(100);
       
-    }
+  }
 // parpadean
     if(messageT == "9 1"){
-      for(int x=0; x<=5; x++){
         digitalWrite(LedS[0],HIGH);
         digitalWrite(LedS[1],HIGH);
         digitalWrite(LedS[2],HIGH);
@@ -185,28 +209,26 @@ void callback(char* topic, byte* message, unsigned int length) {
         digitalWrite(LedS[4],LOW);
         digitalWrite(LedS[5],LOW);
         delay(100);
-      }
     }
 // impar y par
     if(messageT == "10 0"){
-      for(int x=0; x<=5; x++){
         digitalWrite(LedS[0],HIGH);
         digitalWrite(LedS[2],HIGH);
         digitalWrite(LedS[4],HIGH);
-        delay(600);
+        delay(400);
         digitalWrite(LedS[0],LOW);
         digitalWrite(LedS[2],LOW);
         digitalWrite(LedS[4],LOW);
-        delay(600);
+        delay(400);
         digitalWrite(LedS[1],HIGH);
         digitalWrite(LedS[3],HIGH);
         digitalWrite(LedS[5],HIGH);
-        delay(600);
+        delay(400);
         digitalWrite(LedS[1],LOW);
         digitalWrite(LedS[3],LOW);
         digitalWrite(LedS[5],LOW);
-        delay(600);
-      }
+        delay(200);
+      
     }
 
 
@@ -254,37 +276,9 @@ void callback(char* topic, byte* message, unsigned int length) {
     if(messageT == "6 0"){
       digitalWrite(Led6,0);
     }
-  }
-
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("Homero")) {
-      Serial.println("connected");
-      // Subscribe
-      client.subscribe("esp32/homero");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
 
   int btn1state = digitalRead(btn1);
   int btn2state = digitalRead(btn2);
-
 
   int A0state = analogRead(34);  // GPIO34 es un pin ADC
   int A1state = analogRead(35);  // GPIO35 es un pin ADC
@@ -296,4 +290,3 @@ void loop() {
 
   delay(250);
 }
-
